364be57fbe0e1f79e9c8be902a30d143968ee1ad
turanegaku <turanegaku@gmail.com>
2016-09-17 21:38:52 +0900

    express generate
    eslint

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = new express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+    res.render('index', {'title': 'Express'});
+});
+
+module.exports = router;
