4e36da5a13c0b4e958f26ddb7a46431d70668415
turanegaku <turanegaku@gmail.com>
2016-09-20 23:50:45 +0900

    using sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 
 gulp.task('lib', ['js', 'css', 'fonts']);
 
@@ -9,4 +10,14 @@ gulp.task('js', () => {
         'node_modules/moment/min/moment.min.js'
     ])
     .pipe(gulp.dest('static/js'));
+});
+
+gulp.task('sass', () => {
+    return gulp.src('./sass/**/*.scss')
+    .pipe(sass({'outputStyle': 'expanded'}).on('error', sass.logError))
+    .pipe(gulp.dest('./public/stylesheets'));
+});
+
+gulp.task('sass:watch', () => {
+    gulp.watch('./sass/**/*.scss', ['sass']);
 });
