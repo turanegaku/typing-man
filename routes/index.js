030ef3695c9ad44dbc19f880534fc90fe43ea488
turanegaku <turanegaku@gmail.com>
2016-09-20 23:55:37 +0900

    moving man pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const path = require('path');
 const router = new express.Router();
+
+const mans = [];
+
+fs.readdir('./views/mans', (err, files) => {
+    files.forEach((file) => {
+        mans.push(path.basename(file, '.pug'));
+    });
+});
 
 /* GET home page. */
 router.get('/:man', (req, res, next) => {
-    res.render(req.params.man, {'title': 'Express'});
+    res.render('mans/' + req.params.man, {'title': 'Express'});
 });
 
 module.exports = router;
