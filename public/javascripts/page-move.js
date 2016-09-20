ecbb5877152c71bc11a4b1893a636c4b414b3edb
turanegaku <turanegaku@gmail.com>
2016-09-21 00:29:04 +0900

    improve fix bug: movable a cannot move

--- public/javascripts/page-move.js
+++ public/javascripts/page-move.js
@@ -28,37 +28,37 @@ $(() => {
                            .replace(/(.)/g, '<span>$1</span>'));
     });
 
-    const questions = $('#question a:has(span)').children('span:first-child');
+    const questions = $('#question a > span:first-child');
     questions.css({'text-decoration': 'underline'});
 
     questions
     .before('<span></span>');
-    $('#question a:has(span)')
-    .children('span:last-child')
+    $('#question a span:last-child')
     .after('<span></span>');
 
     questions.each((i, q) => {
         let question = $(q);
+        const parent = question.parent();
+        const siblings = parent.children();
+
         $('html').keypress((e) => {
             if (question.text() === String.fromCharCode(e.which)) {
                 question.addClass('done');
-
                 question = question.next();
                 question.css({'text-decoration': 'underline'});
 
                 if (!question.next().length) {              // done sentence
-                    question.parent().attr('href', '#');    // click able
-                    questions.siblings().addBack()
-                    .removeClass('movable');                // remove other
-                    question.siblings().addBack()
-                    .addClass('movable');                   // add self
+                    parent.attr('href', parent.text());     // click able
+                    siblings.removeClass('movable');        // remove other
+                    siblings.addClass('movable');           // add self
                 }
             }
 
             if (isback(e)) {
                 if (question.prev().prev().length) {
-                    question.parent().removeAttr('href')
-                    .children().removeClass('movable');
+                    parent.removeAttr('href');
+                    siblings.removeClass('movable');
+
                     question.css({'text-decoration': 'none'});
                     question = question.prev();
                     question.removeClass('done');
