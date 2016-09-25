ebd21822f4a07dfc07c694d5c76af33d1d283677
turanegaku <turanegaku@gmail.com>
2016-09-26 03:06:20 +0900

    name ranking database system

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ fs.readdir('./views/mans', (err, files) => {
 
 db = new sqlite3.Database('./database.sqlite3');
 db.serialize(() => {
-    db.run('CREATE TABLE result (id TEXT, man TEXT, time INTEGER)',
+    db.run('CREATE TABLE record (date INTEGER, man TEXT, name TEXT, time INTEGER)',
            () => {}); // table exist error cushion
 });
 db.close();
@@ -28,22 +28,74 @@ router.get('/', (req, res, next) => {
 
 router.get('/:man', (req, res, next) => {
     const trg = req.params.man;
+    if (mans.indexOf(trg) < 0) {
+        next();
+        return;
+    }
     const ranks = new Array();
     db = new sqlite3.Database('./database.sqlite3');
-    db.each('SELECT * FROM result', (err, row) => {
-        ranks.push({});
-        console.log(err);
+    // db.each('SELECT * FROM record WHERE man == ?', trg, (err, row) => {
+    //     console.log(row);
+    //     ranks.push({'name': row.name, 'time': moment(row.time).format('mm:ss.SS')});
+    //     if (err) {
+    //         console.error(err);
+    //     }
+    // });
+    db.all('SELECT * FROM record WHERE man == ? ORDER BY time, date DESC LIMIT 10', trg, (err, rows) => {
+        if (err) {
+            console.error(err);
+            res.end('error');
+            return;
+        }
+        rows.forEach(row => {
+            ranks.push({'name': row.name, 'time': moment(row.time).format('mm:ss.SS')});
+        });
+
+        while (ranks.length < 10) {
+            ranks.push({'name': 'NoData', 'time': moment(30 * 60 * 1000).format('mm:ss.SS')});
+        }
+
+        res.render('mans/' + trg, {
+            'title': 'typing-man',
+            'ranks': ranks,
+            'name': req.cookies.name
+        });
     });
     db.close();
+});
 
-    while (ranks.length < 10) {
-        ranks.push({'name': 'NoData', 'time': moment(30 * 60 * 1000).format('mm:ss.SS')});
+router.post('/:man', (req, res, next) => {
+    const trg = req.params.man;
+    if (mans.indexOf(trg) < 0) {
+        res.end('invarid');
+        return;
     }
 
-    if (mans.indexOf(trg) >= 0) {
-        res.render('mans/' + trg, {'title': 'typing-man', 'ranks': ranks});
+    const name = req.body.name;
+    const time = moment(req.body.time, 'mm:ss.SS') - moment('0', 'm');
+    console.log(trg, name, time);
+    let valid = true;
+    if (name.length > 12 || time < 0) {
+        valid = false;
+    }
+
+    if (valid) {
+        db = new sqlite3.Database('./database.sqlite3');
+        db.run('INSERT INTO record VALUES(?, ?, ?, ?)', moment().unix(), trg, name, time, (err) => {
+            if (err) {
+                console.error(err);
+                valid = false;
+            }
+        });
+        db.close();
+    }
+
+    if (valid) {
+        res.cookie('name', name);
+        res.end('ok');
     } else {
-        next();
+        res.end('invalid');
+        return;
     }
 });
 
