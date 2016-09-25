fb3a4c639fd96ffff2b99c428781d0553ac5a89f
turanegaku <turanegaku@gmail.com>
2016-09-26 00:15:11 +0900

    sqlite database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const moment = require('moment');
+const sqlite3 = require('sqlite3').verbose();
 const router = new express.Router();
 
 const mans = new Array();
+let db;
 
 fs.readdir('./views/mans', (err, files) => {
     files.forEach((file) => {
@@ -11,15 +14,34 @@ fs.readdir('./views/mans', (err, files) => {
     });
 });
 
+db = new sqlite3.Database('./database.sqlite3');
+db.serialize(() => {
+    db.run('CREATE TABLE result (id TEXT, man TEXT, time INTEGER)',
+           () => {}); // table exist error cushion
+});
+db.close();
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
     res.render('index', {'title': 'typing-man', 'mans': mans});
 });
 
 router.get('/:man', (req, res, next) => {
-    console.log(req.params.man in mans);
-    if (mans.indexOf(req.params.man) >= 0) {
-        res.render('mans/' + req.params.man, {'title': 'typing-man'});
+    const trg = req.params.man;
+    const ranks = new Array();
+    db = new sqlite3.Database('./database.sqlite3');
+    db.each('SELECT * FROM result', (err, row) => {
+        ranks.push({});
+        console.log(err);
+    });
+    db.close();
+
+    while (ranks.length < 10) {
+        ranks.push({'name': 'NoData', 'time': moment(30 * 60 * 1000).format('mm:ss.SS')});
+    }
+
+    if (mans.indexOf(trg) >= 0) {
+        res.render('mans/' + trg, {'title': 'typing-man', 'ranks': ranks});
     } else {
         next();
     }
