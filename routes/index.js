857405f1dd82da7938af8767636da988a92450c4
turanegaku <turanegaku@gmail.com>
2016-10-01 02:31:10 +0900

    use postgre
    use config

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,34 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const moment = require('moment');
-const sqlite3 = require('sqlite3').verbose();
 const socket = require('../module/socket.js');
 const router = new express.Router();
+
+const url = require('url');
+
+const Pool = require('pg').Pool;
+const params = url.parse(process.env.DATABASE_URL || require('config').get('DATABASE_URL'));
+const auth = params.auth.split(':');
+const pool = new Pool({
+    'user': auth[0],
+    'password': auth[1],
+    'host': params.hostname,
+    'port': params.port,
+    'database': params.pathname.split('/')[1],
+    'ssl': true,
+    'max': 20,
+    'idleTimeoutMillis': 1000,
+});
 
 const rankMAX = 10;
 
 const mans = new Array();
-let db;
 
 fs.readdir('./views/mans', (err, files) => {
     files.forEach((file) => {
         mans.push(path.basename(file, '.pug'));
     });
 });
-
-db = new sqlite3.Database('./database.sqlite3');
-db.serialize(() => {
-    db.run('CREATE TABLE record (date INTEGER, man TEXT, name TEXT, time INTEGER, error INTEGER)',
-           () => {}); // table exist error cushion
-});
-db.close();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -37,15 +44,16 @@ router.get('/:man', (req, res, next) => {
     }
 
     const ranks = new Array(rankMAX);
-    db = new sqlite3.Database('./database.sqlite3');
-    db.all('SELECT * FROM record WHERE man == ? ORDER BY time, error, date DESC LIMIT 10', trg, (err, rows) => {
+
+    const q = 'SELECT * FROM record WHERE man = $1::text ORDER BY time, error, date DESC LIMIT $2::int;';
+    pool.query(q, [trg, rankMAX], (err, ret) => {
         if (err) {
             console.error(err);
             res.end('error');
             return;
         }
 
-        rows.forEach((row, i) => {
+        ret.rows.forEach((row, i) => {
             ranks[i] = {
                 'name': row.name,
                 'time': row.time,
@@ -53,7 +61,7 @@ router.get('/:man', (req, res, next) => {
                 'error': row.error,
             };
         });
-        for (let i = rows.length; i < ranks.length; ++i) {
+        for (let i = ret.rows.length; i < ranks.length; ++i) {
             ranks[i] = {
                 'name': 'NoData',
                 'time': 30 * 60 * 1000,
@@ -72,7 +80,6 @@ router.get('/:man', (req, res, next) => {
 
         res.render('mans/' + trg, arg);
     });
-    db.close();
 });
 
 router.post('/:man', (req, res, next) => {
@@ -90,14 +97,13 @@ router.post('/:man', (req, res, next) => {
     }
 
     if (valid) {
-        db = new sqlite3.Database('./database.sqlite3');
-        db.run('INSERT INTO record VALUES(?, ?, ?, ?, ?)', moment().unix(), trg, req.body.name, req.body.time, req.body.error, (err) => {
+        const q = 'INSERT INTO record VALUES($1, $2, $3, $4, $5)';
+        pool.query(q, [moment().unix(), trg, req.body.name, req.body.time, req.body.error], (err, ret) => {
             if (err) {
                 console.error(err);
                 valid = false;
             }
         });
-        db.close();
     }
 
     if (valid) {
