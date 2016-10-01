80cb78f33b81ff145456ee3a92419df09ac43625
turanegaku <turanegaku@gmail.com>
2016-10-02 04:44:16 +0900

    key board layout button

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const users = require('./routes/users');
 
 const app = express();
 
+app.locals.title = 'typing-man';
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
