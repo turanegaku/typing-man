0ef91b3d6672e4186ac53dc8e10346ca4225aebf
turanegaku <turanegaku@gmail.com>
2026-05-31 20:16:13 +0900

    feat: add JetBrains Mono font

--- src/templates/layout.tsx
+++ src/templates/layout.tsx
@@ -14,6 +14,9 @@ export const Layout: FC<LayoutProps & { children?: any }> = ({
     <html>
       <head>
         <title>{title}</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="/stylesheets/style.css" />
       </head>
       <body>
